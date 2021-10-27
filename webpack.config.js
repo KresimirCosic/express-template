946d17a298170c1bfa465ef6292cc4b8c6db8f8e
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2021-10-27 16:57:32 +0200

    webpack setup basic webpack setup; test it works and reloads properly

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpackNodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  context: __dirname,
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js',
+    publicPath: '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  externals: [webpackNodeExternals],
+};
